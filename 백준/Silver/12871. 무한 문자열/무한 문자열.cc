4c6e51c94874ab9bffb2cc22670afe521bf91f6d
Kitae Kim <99324370+jason9288@users.noreply.github.com>
2025-08-01 21:13:44 +0900

    [Silver V] Title: 무한 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/12871. 무한 문자열/무한 문자열.cc
+++ 백준/Silver/12871. 무한 문자열/무한 문자열.cc
@@ -5,15 +5,6 @@ int main() {
     string a, b;
     cin >> a >> b;
 
-    string s1 = "", s2 = "";
-
-    for (int i = 0; i < a.length(); i++) {
-        s1 += b;
-    }
-    for (int i = 0; i < b.length(); i++) {
-        s2 += a;
-    }
-
-    if (s1 == s2) cout << 1;
-    else cout << 0;
+    if (a + b != b + a) cout << 0;
+    else cout << 1;
 }
