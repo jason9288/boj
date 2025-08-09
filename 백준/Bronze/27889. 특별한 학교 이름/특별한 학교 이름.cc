bca0deb907529234115eba00a8826460a2db3fee
Kitae Kim <99324370+jason9288@users.noreply.github.com>
2025-08-09 19:26:05 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a; cin >> a;
+    
+    if (a == "NLCS") cout << "North London Collegiate School";
+    if (a == "BHA") cout << "Branksome Hall Asia";
+    if (a == "KIS") cout << "Korea International School";
+    if (a == "SJA") cout << "St. Johnsbury Academy";
+}
