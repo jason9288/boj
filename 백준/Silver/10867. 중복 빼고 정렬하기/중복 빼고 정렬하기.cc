b78348a5af8851506747117160a60b92018bf677
Kitae Kim <99324370+jason9288@users.noreply.github.com>
2025-08-25 21:13:05 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 12 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] " << #c << ": "; for (const auto& elem : c) cout << elem << " "; cout << '\n'; }
+//=========================================================================================================
+
+int main() {
+    FAST_IO
+
+    int n; cin >> n;
+    set<int> ss;
+
+    for (int i = 0; i < n; i++){
+        int k; cin >> k;
+        ss.insert(k);
+    }
+
+    for (int i : ss) {
+        cout << i << " ";
+    }
+
+}
